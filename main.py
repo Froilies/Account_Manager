72f1858c99c7af53b07d5a09650dfeb6bdcfde5b
Froilies <122579919+Froilies@users.noreply.github.com>
2023-01-26 19:13:31 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+import base64
+import os
+from cryptography.fernet import Fernet
+
+def Prog_pass():
+    key = input("What is the Program key")
+    return key
+
+#First, Create your own private key using this function and store securely
+"""def create_key():
+    key = Fernet.generate_key()
+    with open("secure.key", 'wb') as key_file:
+        key_file.write(key)"""
+
+
+def view():
+    print("The existing accounts in the program are as follows:\n")
+    with open("accounts.txt", "r") as file:
+        for line in file.readlines():
+            data = line.rstrip()
+            website, username, pwd = data.split("  ||  ")
+            print(f"Website: {website}, Username: {username}, Password: {pwd}")
+
+
+def add():
+    website = input("What website is the account connected to: ")
+    username = input("Account log in: ")
+    pwd = input("Account password: ")
+
+    with open("accounts.txt", "a") as file:
+        file.write(website + "  ||  "+ username + "  ||  "+ pwd + "\n")
+
+def delete():
+    website = input("What website is the account connected to: ")
+    username = input("Account log in: ")
+    pwd = input("Account password: ")
+
+    with open("accounts.txt", "a") as file:
+        file.write(website + "  ||  "+ username + "  ||  "+ pwd + "\n")
+
+
+def main():
+    print("Account accessed! What would you like to do")
+
+    while True:
+        selection = input("Pick a selection: Add: Add an account \n"
+                          "View: View existing accounts, \n"
+                          "Delete: Delete an existing account or \n"
+                          "Quit: Exit the Program\n").lower()
+
+        if selection == "quit" or selection == "q":
+            print("This program will now close")
+            break
+        if selection == "add" :
+            add()
+        elif selection == "view" :
+            view()
+        elif selection == "delete" :
+            delete()
+        else:
+            print("invalid selection")
+            continue
+
+
+if Prog_pass() == "gypG3JOXyKKrSi3EJ2_gNYoAVqo2FT5P7rF1M3_3d-M=":
+    main()
+else:
+    print("Wrong key")
